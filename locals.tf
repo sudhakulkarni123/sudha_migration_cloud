c27c5680f301b6a94725ad60c6b24175ecb7acac
sudhakulkarni123 <sudha.kulkarni@cloudreach.com>
2023-05-23 17:29:53 +0530

    change az

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  availability-zones      = slice(data.aws_availability_zones.available.names, 0, var.number_of_azs)
+  availability-zones      = slice(data.aws_availability_zones.available.names, 0,2)
   public_subnet_cidr      = cidrsubnet(var.vpc_cidr, 1, 0)
   private_subnet_cidr     = cidrsubnet(var.vpc_cidr, 1, 1)
   database_subnet_cidr    = cidrsubnet(var.vpc_cidr, 2, 1)
