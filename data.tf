cedbbc5243e823f1ff52093efc3aa6c8d9854a24
sudhakulkarni123 <sudha.kulkarni@cloudreach.com>
2023-05-25 15:51:29 +0530

    tflint added

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
 
 
--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  required_version = ">= 1.1.0"
+}
