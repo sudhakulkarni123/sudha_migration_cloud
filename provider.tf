6df622958bbb0f01d5259cbbfdb3fcbe14141997
sudhakulkarni123 <sudha.kulkarni@cloudreach.com>
2023-05-25 15:52:35 +0530

    tflint added 1

--- provider.tf
+++ provider.tf
@@ -4,3 +4,11 @@ provider "aws" {
 terraform {
   required_version = ">= 1.1.0"
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.66.1 "
+    }
+  }
+}
